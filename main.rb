1cbf73430b85fa3c091c82462c1c0bd30056b684
Peter Huynh <peterhyh@yahoo.com>
2024-11-26 16:55:46 -0600

    name analyzer for lesson 1 complete

--- main.rb
+++ main.rb
@@ -1,4 +1,10 @@
-first_name = "Peter"
-last_name = "Huynh"
+puts "Enter your first name"
+first_name = gets.chomp
+puts "Enter your last name"
+last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+name_char = full_name.length
 
-puts "Hi! My name is #{first_name} #{last_name}"
+puts "Your full name is #{full_name}"
+puts "Your name in reverse is #{full_name.reverse}"
+puts "Your name has #{name_char} characters in it"
